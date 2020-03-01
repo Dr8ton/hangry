b509c5fc85c34bd24a88a8a701e3e6d3d02cc53b
Drayton Kittel <drayton.kittel@gmail.com>
2020-03-01 17:37:15 -0600

    Removed excess links

--- resources/views/inc/banner.blade.php
+++ resources/views/inc/banner.blade.php
@@ -1,12 +1,13 @@
-<div class="jumbotron text-center">
+<div class="container text-center">
     <h1> Shopping List</h1> 
     {!! Form::open(['action' => 'ItemsController@store', 'method' => 'POST']) !!}
     
     <div class="form-group">
         {{Form::label('title','What Shall we buy?')}}
         {{Form::text('title', '', ['class' => 'form-control'])}}
+        {{-- //TODO:  don't want this so wide. 'wide as the google bar per the wife' --}}
     </div>
 
-    {{Form::submit('Submit',['class'=> 'btn btn-primary'])}}
+    {{Form::submit('Add to list',['class'=> 'btn btn-primary'])}}
     {!! Form::close() !!}
 </div>
