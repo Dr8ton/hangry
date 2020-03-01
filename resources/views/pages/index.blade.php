59f7ac573b95d91016b451d2c7ec5a0b63626b9f
Drayton Kittel <drayton.kittel@gmail.com>
2020-02-29 22:50:23 -0600

    moved text into jumbotron

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,9 +2,9 @@
 
 
 @section('content')
-    <div class="jumbotron text-center"></div>
+    <div class="jumbotron text-center">
         <h1> Don't shop hungry</h1> 
-        <p><a href="#" class="btn btn-primary btn-lg"></a>+ Add Item</p>
+        <p><a href="#" class="btn btn-primary btn-lg"></a>+Add Item</p>
     </div>
 @endsection
 
