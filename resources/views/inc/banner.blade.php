ac73fd36a58aa889435e5c15cacf4657da0f0f65
Drayton Kittel <drayton.kittel@gmail.com>
2020-03-01 15:33:08 -0600

    moved create form to banner

--- resources/views/inc/banner.blade.php
+++ resources/views/inc/banner.blade.php
@@ -1,3 +1,12 @@
 <div class="jumbotron text-center">
     <h1> Shopping List</h1> 
+    {!! Form::open(['action' => 'ItemsController@store', 'method' => 'POST']) !!}
+    
+    <div class="form-group">
+        {{Form::label('title','What Shall we buy?')}}
+        {{Form::text('title', '', ['class' => 'form-control'])}}
+    </div>
+
+    {{Form::submit('Submit',['class'=> 'btn btn-primary'])}}
+    {!! Form::close() !!}
 </div>
