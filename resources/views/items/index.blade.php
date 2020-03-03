1004b62fbd1de936a2871de7092cedb87f09acbd
Drayton Kittel <drayton.kittel@gmail.com>
2020-03-02 23:39:21 -0600

    modal populates with title and id

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -6,7 +6,7 @@
     <div class="list-group">
         @foreach($items as $item)
             <li class="list-group-item">{{$item->title}}
-            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal" data-dog="{{$item->id}}">Edit</button>
+            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal" data-item-id="{{$item->id}}" data-item-title="{{$item->title}}">Edit</button>
             </li>
         @endforeach
     </div>
@@ -19,26 +19,23 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Edit Item</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              <form>
+              <form action="/items/update" method="POST">
                 <div class="form-group">
-                  <label for="recipient-name" class="col-form-label">Recipient:</label>
-                  <input type="text" class="form-control" id="recipient-name">
-                </div>
-                <div class="form-group">
-                  <label for="message-text" class="col-form-label">Message:</label>
-                  <textarea class="form-control" id="message-text"></textarea>
+                  <label for="item-title" class="col-form-label">Recipient:</label>
+                  <input type="text" class="form-control" id="item-title">
+                  <input type="hidden" name="item-id" id="item-id" value="">
                 </div>
               </form>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Send message</button>
+              <button type="submit" class="btn btn-primary">Update item</button>
             </div>
           </div>
         </div>
@@ -47,13 +44,12 @@
  <script>
      $('#exampleModal').on('show.bs.modal', function (event) {
          console.log('modal open');
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var recipient = button.data('dog') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var button = $(event.relatedTarget) 
+        var id_value = button.data('item-id') 
+        var item_title = button.data('item-title') 
         var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
+        modal.find('.modal-body #item-id').val(id_value)
+        modal.find('.modal-body #item-title').val(item_title)
 })
  </script>
 
